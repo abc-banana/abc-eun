f15dcad0405e1746fda16ff72ee41471e019d518
EunOh <bdohhhhh@gmail.com>
2025-10-01 11:56:10 +0900

    add: auth - user route

---
+++ app/api/auth/user/route.ts
@@ -0,0 +1,14 @@
+import { NextResponse } from "next/server";
+import { createClient } from "@/lib/supabase/server";
+
+export async function GET() {
+  const supabase = await createClient();
+
+  const { data: user, error } = await supabase.auth.getUser();
+
+  if (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+
+  return NextResponse.json(user.user, { status: 200 });
+}
