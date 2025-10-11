7d5e58235147ee6f98b9b40a6fd6238f2bb59b21
EunOh <bdohhhhh@gmail.com>
2025-10-11 14:28:06 +0900

    update: apis.ts

--- src/apis/apis.ts
+++ src/apis/apis.ts
@@ -1,4 +1,6 @@
 import { User } from "@supabase/supabase-js";
+import { ApiConfig } from "@/types/api-types";
+import { NanoBanana } from "@/types/types";
 import { BaseApiClient } from "./base-axios-client";
 
 class ApiClient extends BaseApiClient {
@@ -22,13 +24,59 @@ class ApiClient extends BaseApiClient {
 
   // == 도메인 메서드 (AbortSignal 지원) ==
   // 여기에 계속 추가...
-  public getUser() {
+  public getUser = () => {
     return this.get<User>("/api/auth/user");
-  }
+  };
 
-  public logOut() {
-    return this.get("/api/auth/logout");
-  }
+  public logOut = () => {
+    return this.delete<void>("/api/auth/logout");
+  };
+
+  // 이미지 생성
+  public postGenMemorialPhoto = (image: File) => {
+    const formData = new FormData();
+    formData.append("image", image);
+
+    return this.post<{ base64Image: string }>("/api/gen", formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+      timeout: 120000, // 이미지 생성은 오래 걸릴 수 있으므로 타임아웃을 2분으로 설정
+    });
+  };
+
+  // 생성된 이미지 저장
+  public postSaveMemorialPhoto = (image: File, category: string) => {
+    const formData = new FormData();
+    formData.append("image", image);
+
+    return this.post<{ base64Image: string }>("/api/image", formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+      params: {
+        category,
+      },
+    });
+  };
+
+  // 하나의 특정한 이미지 조회
+  public getMemorialPhoto = (id: string, category: string) => {
+    return this.get<NanoBanana>(`/api/image/${id}`, {
+      params: {
+        category,
+      },
+    });
+  };
+
+  // 유저의 모든 이미지 조회
+  public getMemorialPhotos = (category: string) => {
+    return this.get<NanoBanana[]>(`/api/image`, {
+      params: {
+        category,
+      },
+    });
+  };
 }
 
 const api = new ApiClient();
