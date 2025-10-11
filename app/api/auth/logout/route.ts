7b22bc4ff61d5717c084d058664001bd7f049d7e
EunOh <bdohhhhh@gmail.com>
2025-10-11 10:22:17 +0900

    Update: logout route 변경

--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -2,7 +2,7 @@ import { redirect } from "next/navigation";
 import { NextResponse } from "next/server";
 import { createClient } from "@/lib/supabase/server";
 
-export async function GET() {
+export async function DELETE() {
   const supabase = await createClient();
 
   const { error } = await supabase.auth.signOut();
